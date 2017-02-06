b43000f516da4dc58e12bf6851f3c3ab5b3d2c2e
Alexandre Tobia <alexandre.tobia@gmail.com>
2017-02-06 18:02:01 +0100

    Correction exo 1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,43 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+
+
+
+
+//EXO1
+
+/*Route::get('/iim', function() {
+   return view('exo1.iim');
+});
+
+
+Route::get('/bonjour/{name}', function($name) {
+    return view('exo1.bonjour', ['prenom' => $name]);
+});
+
+Route::get('/test', function() {
+    $age = 15;
+
+    $tasks = [
+        'Faire le ménage',
+        'Envoyer un mail'
+    ];
+
+    return view('exo1.test', compact('age', 'tasks'));
+});
+
+Route::get('/page1', function() {
+    return view('exo1.page1');
+});
+
+Route::get('/page2', function() {
+    return view('exo1.page2');
+});*/
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
